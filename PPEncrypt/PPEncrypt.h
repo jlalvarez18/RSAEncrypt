5c03b97b99d976fd4a3fd1ac13841ba50a021bdf
Juan <juan@Quirkys-MacBook-Pro-2.local>
2014-03-03 14:30:11 -0500

    Initial Commit

---
+++ PPEncrypt/PPEncrypt.h
@@ -0,0 +1,13 @@
+//
+//  PPEncrypt.h
+//  PPEncrypt
+//
+//  Created by Juan on 3/3/14.
+//  Copyright (c) 2014 Alvarez Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PPEncrypt : NSObject
+
+@end
---
+++ PPEncrypt/PPEncrypt.m
@@ -0,0 +1,13 @@
+//
+//  PPEncrypt.m
+//  PPEncrypt
+//
+//  Created by Juan on 3/3/14.
+//  Copyright (c) 2014 Alvarez Productions. All rights reserved.
+//
+
+#import "PPEncrypt.h"
+
+@implementation PPEncrypt
+
+@end
